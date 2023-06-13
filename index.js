792783da42e79682cba69c3f80097a6bd53b175a
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-06-13 13:51:20 -0300

    03

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const historico = document.getElementById('historico')
 const res = document.getElementById('equal-btn')
 const previos = document.getElementById('previos')
 
-
-let numerosInseridos = []
 let contador = 0
 let contRes = 0
-
-function insert(num) {
-
-    tela.innerHTML += num
-    previos.innerHTML += num
-
-    numerosInseridos.push(num)
-
-    contador = 0
-    contRes = 0
-}
 
 if (contador < 0 || contador === 2) {
     contador = 0
 }
+
+function insert(num) {
+
+    tela.innerHTML += num
+    previos.innerHTML = '=' + eval(tela.innerHTML)
+    
+    if(tela.innerHTML.length > 1){
+        historico.innerHTML += tela.innerHTML + '<br>'
+    }
+    contador = 0
+    contRes = 0
+}
+
 function clean() {
     previos.innerHTML = ''
     tela.innerHTML = ''
     contador++
-    if (contador > 2) {
+    if (contador >= 2) {
         historico.innerHTML = ''
         contador = 0
         contRes = 0
@@ -46,19 +46,19 @@ function back() {
 function calcular() {
     var resultado = document.getElementById('tela').innerHTML;
 
-
     if (resultado) {
         tela.innerHTML = eval(resultado)
-        previos.innerHTML = eval(numerosInseridos.join(''))
-
+        
+        historico.innerHTML += '=' + tela.innerHTML + '<br>'
+    
         contRes++
         contador = 0
 
         if (contRes === 2) {
             tela.innerHTML = ''
+            previos.innerHTML = ''
             contRes = 0
         }
     }
 }
-
 
